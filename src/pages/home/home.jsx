f3af3583ca3b4e4646f143f82f4a4b2781f0d595
Ethan Mylett <ethan.mylett@yahoo.com>
2025-03-17 16:04:10 -0400

    added more messages

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,14 +2,21 @@ import { Id_Badge } from "../../components/Id_Card";
 import { Typewriter } from '../../components/Typewriter';
 import "./home.css";
 
-const typewriter_messages = [
-  '',
+const typewriterMessages = [
+  'welcome to Ethan\'s portfolio',
+  'building smarter software',
+  'in my element - <div>',
+  'machine learning enthusiast',
+  'the picasso of front-end',
+  'turning ☕ into 💻',
+  'data-driven decision maker',
+  'breaking loops, not promises'
 ]
 
 export const Home = () => {
   return (
     <div className="home-page">
-      <Typewriter texts={['hello friends', 'my name is ethan']}/>
+      <Typewriter texts={typewriterMessages}/>
       <Id_Badge />
     </div>
   );
