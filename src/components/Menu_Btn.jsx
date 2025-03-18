a0b4b45d1ab204aefa3b4df7516586ad38b5629d
Ethan Mylett <ethan.mylett@yahoo.com>
2025-03-17 22:03:31 -0400

    remove some dividers in menu

--- src/components/Menu_Btn.jsx
+++ src/components/Menu_Btn.jsx
@@ -16,7 +16,6 @@ export const Menu = () => {
         Menu
       </button>
       <aside className={`menu-link-list ${isOpen ? " open" : ""}`}>
-        <div className="menu-divider"></div>
         <Link className="menu-link" to="/">
           <span className="link-number">01.</span>
           <span className="link-name">Projects</span>
@@ -31,7 +30,6 @@ export const Menu = () => {
           <span className="link-number">03.</span>
           <span className="link-name">Projects</span>
         </Link>
-        <hr className="menu-divider"></hr>
       </aside>
 
       <Outlet />
