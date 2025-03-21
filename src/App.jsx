5e1ab4cf945ee478abdf43dc136224d82e1ae809
Ethan Mylett <ethan.mylett@yahoo.com>
2025-03-21 11:33:11 -0400

    about page planning

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/home";
-import { About } from "./pages/about/about";
+import { About } from "./pages/about/About";
 import "./index.css";
 
 function App() {
--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,5 +1,21 @@
 import { Header } from "../../components/Header/Header";
 
 export const About = () => {
-  return <Header pages={["home", "projects", "contact"]} />;
+  return (
+    <>
+      <Header pages={["home", "projects", "contact"]} />
+      <div className="about-info">
+        {/** animated element
+         * About
+         * short desc bold
+         * photo
+         */}
+        <p className="about-me-intro">{/** paragraph(s) */}</p>
+        <p className="about-me-quote">{/** quote  */}</p>
+        <p className="about-me-experience">{/** exp */}</p>
+        <p className="about-me-skills">{/** skills*/}</p>
+        <div className="about-me-resume">{/** resume */}</div>
+      </div>
+    </>
+  );
 };
