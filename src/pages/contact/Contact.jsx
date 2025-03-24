c5f5fd71481da7d9927f0dcec83106d0a1d2ad79
Ethan Mylett <ethan.mylett@yahoo.com>
2025-03-23 21:10:12 -0400

    contact page styling

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,82 @@
 import { Header } from "../../components/Header/Header";
 import { Logo } from "../../components/Logo";
+import "./Contact.css";
+
 export const Contact = () => {
   return (
     <>
       <Header pages={["home", "projects", "about"]} />
-      <Logo />
+      <div className="text-input-container">
+        <div className="text-container">
+          <section>
+            <h2>Lets Talk.</h2>
+            <p>
+              Looking for a software engineer to join your company? Feel free to
+              reach out so we can discuss how I can drive business outcomes at
+              your company!
+            </p>
+          </section>
+          <section>
+            <h4>Email</h4>
+            <p>ethan.mylett@yahoo.com</p>
+          </section>
+          <section>
+            <h4>Check Me Out</h4>
+            <a
+              href="https://linkedin.com/in/mylett"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://github.com/ethanm272"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </section>
+        </div>
+        <form action="#" className="contact-form">
+          <section className="single-input-container">
+            <label htmlFor="fname">First Name</label>
+            <input
+              className="form-input"
+              type="text"
+              id="fname"
+              name="fname"
+              required
+            />
+          </section>
+          <section className="single-input-container">
+            <label htmlFor="email">Email</label>
+            <input
+              className="form-input"
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
+          </section>
+          <section className="single-input-container">
+            <label htmlFor="message">Type your message here</label>
+            <input
+              className="form-input"
+              type="text"
+              id="message"
+              name="message"
+            />
+          </section>
+          <button
+            type="submit"
+            value="Submit"
+            className="submit-btn btn-layered-3d btn-layered-3d--color"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 };
